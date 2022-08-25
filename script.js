03fde6acf609fe8af44bd6182ba431faa1f73c9a
Esperlluette <janl.mannessier@gmail.com>
2022-08-25 13:24:28 +0200

    fix

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
-var ARNm = document.getElementById('inputARNm');
-var btn = document.getElementById('click');
+let ARNm = document.getElementById('inputARNm');
+let btn = document.getElementById('click');
 const regex = new RegExp('[A|U|C|G]{3}');
+
+
+console.table()
 
 if (btn != null) {
     btn.addEventListener('click', () => onClick(ARNm), false);
 }
 
 function onClick(ARNm) {
-    var value = ARNm.value;
-
+    let value = ARNm.value;
     if (regex.test(value)) {
         console.log(true);
     } else {
         console.log(false);
         return;
     }
-
-
     if (canBeSlice(value)) {
         slicer3(value);
     }
-
 }
 
 function canBeSlice(value) {
-    var bool = true;
+    let bool = true;
     if (value.length % 3 != 0) {
         alert("ne peut pas être slicer");
         bool = false;
@@ -45,28 +44,104 @@ function slicer3(value) {
     **/
 
     console.log(value);
-    var loop = value.length / 3
-    var iteration = 0;
-    var triplet = [];
-    var temp = [];
+    let loop = value.length / 3
+    let triplet = [];
+    let temp = [];
     for (let i = 1; i <= loop*3; i++) {
-
-        temp.push(value[iteration + i - 1])
-
-        
+        temp.push(value[i - 1])
         if (i % 3 == 0 && i != 1) {
-            // console.log(temp);
-            iteration++;
             triplet.push(temp);
             temp = [];
         }
-
-        
     }
     console.log(triplet);
+}
 
+function decode(triplet) {
+    
 }
 
 
 
 
+const dCode = {
+    "UUU": "phénylalanine",
+    "UUC": "phénylalanine",
+    "UUA": "Leucine",
+    "UUG": "Leucine",
+
+    "UCU": "Serine",
+    "UCC": "Serine",
+    "UCA": "Serine",
+    "UCG": "Serine",
+
+    "UAU": "Tyrosine",
+    "UAC": "Tyrosine",
+    "UAA": "STOP",
+    "UAG": "STOP",
+
+    "UGU": "Cystéine",
+    "UGC": "Cystéine",
+    "UGA": "STOP",
+    "UGG": "Tryptophane",
+
+    "CUU": "Leucine",
+    "CUC": "Leucine",
+    "CUA": "Leucine",
+    "CUG": "Leucine",
+
+    "CCU": "Proline",
+    "CCC": "Proline",
+    "CCA": "Proline",
+    "CCG": "Proline",
+
+    "CAU": "Histidine",
+    "CAC": "Histidine",
+    "CAA": "Glutamine",
+    "CAG": "Glutamine",
+
+    "CGU": "Arginine",
+    "CGC": "Arginine",
+    "CGA": "Arginine",
+    "CGG": "Arginine",
+
+    "AUU": "Isoleucine",
+    "AUC": "Isoleucine",
+    "AUA": "Isoleucine",
+    "AUG": "Méthionine",
+
+    "ACU": "Thréonine",
+    "ACC": "Thréonine",
+    "ACA": "Thréonine",
+    "ACG": "Thréonine",
+
+    "AAU": "Asparagine",
+    "AAC": "Asparagine",
+    "AAA": "Lysine",
+    "AAG": "Lysine",
+
+    "AGU": "Serine",
+    "AGC": "Serine",
+    "AGA": "Arginine",
+    "AGG": "Arginine",
+
+    "GUU": "Valine",
+    "GUC": "Valine",
+    "GUA": "Valine",
+    "GUG": "Valine",
+
+    "GCU": "Alanine",
+    "GCC": "Alanine",
+    "GCA": "Alanine",
+    "GCG": "Alanine",
+
+    "GAU": "Aspartate",
+    "GAC": "Aspartate",
+    "GAA": "Glutamate",
+    "GAG": "Glutamate",
+
+    "GGU": "Glycine",
+    "GGC": "Glycine",
+    "GGA": "Glycine",
+    "GGG": "Glycine"
+}
